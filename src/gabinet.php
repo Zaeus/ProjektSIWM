3bad9bdef5996476c3b434a444498e2c1b09d8a2
Zaeus <wpb1992@gmail.com>
2015-05-18 17:11:51 +0200

    Wersja 3
    Wersja tabeli gabinetów

--- src/gabinet.php
+++ src/gabinet.php
@@ -6,13 +6,53 @@
 	include("polaczenieSQL.php");
 	include("kwerenda_log.php");
 ?>
-//Edycja gabinetów
+//Edycja gabinetów<br>
 
 <?
 	if(isset($_SESSION['login']) && ($_SESSION['haslo'] == $hasloSql)){
 		if($_SESSION['uprawnienia'] >= 1) {
-			echo "Posiadasz uprawnienia lekarza";
-			//Edycja gabinetów
+			echo "Posiadasz uprawnienia lekarza<br>"; // To do późniejszego wywalenia
+			// TODO Dorobić wyświetlenie aktualnej daty (zakresu dat dla tygodnia)
+			// TODO Dorobić przyciski tydzień w przód/tył
+			?>
+			<br><table cellpadding="5" border="1">
+			<tr bgcolor>
+			<td><center>Godzina</center></td>
+			<td><center>Poniedziałek</center></td>
+			<td><center>Wtorek</center></td>
+			<td><center>Środa</center></td>
+			<td><center>Czwartek</center></td>
+			<td><center>Piątek</center></td>
+			</tr>
+			<?
+			$Godzina = 8;
+			// Dat nie ruszać bo się zjebią
+			$daty = gmdate("H:i", 28800);	
+			$half = gmdate("H:i", 1800);
+			$half = strtotime(gmdate("H:i", 1800)) - strtotime("00:00");
+			while($Godzina < 35) {
+				// TODO Kwerendy z zapytaniami dla danej godziny dla danego dnia
+				// TODO Kolorowanie(?) wolnych/zajętych miejsc - z wypisaniem nazwiska lekarza zajmującego gabinet
+				?>
+				<tr bgcolor=white>
+				<td width="100"><center> 
+				<?
+				echo $daty;
+				?>
+				</center></td>
+				<td width="100"><center>A</center></td>
+				<td width="100"><center>B</center></td>
+				<td width="100"><center>C</center></td>
+				<td width="100"><center>D</center></td>
+				<td width="100"><center>E</center></td>
+				</tr>
+				<?
+				$Godzina = $Godzina + 1;
+				$daty = date("H:i", strtotime($daty ) + $half);
+			}			
+			?>
+			</table>
+			<?
 		}
 		else {
 			echo "Nie posiadasz uprawnień lekarza";
