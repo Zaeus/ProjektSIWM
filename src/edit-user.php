ad52e155c84e607f0ceca313edbfd8f2841798fd
Zaeus <wpb1992@gmail.com>
2015-06-01 20:25:59 +0200

    Ogarnianie v3

--- src/edit-user.php
+++ src/edit-user.php
@@ -13,68 +13,68 @@
 
             // Update danych w bazie SQL
             if(isset($_POST['nowe_haslo']) || isset($_POST['nowy_email']) || isset($_POST['nowe_nazwisko'])){
-                $kwerenda_edycji = "UPDATE nazwiska SET email='" . $_POST['nowy_email'] . "', ";
-                $kwerenda_edycji .= "nazwisko='" . $_POST['nowe_nazwisko'] . "', ";
-                $kwerenda_edycji .= "imie='" . $_POST['nowe_imie'] . "', ";
-                $kwerenda_edycji .= "specjalizacja=";
+                $editQuery = "UPDATE nazwiska SET email='" . $_POST['nowy_email'] . "', ";
+                $editQuery .= "nazwisko='" . $_POST['nowe_nazwisko'] . "', ";
+                $editQuery .= "imie='" . $_POST['nowe_imie'] . "', ";
+                $editQuery .= "specjalizacja=";
                 if($_POST['nowa_specjalizacja'] == ""){
-                    $kwerenda_edycji .= "NULL ";
+                    $editQuery .= "NULL ";
                 }
                 else {
-                    $kwerenda_edycji .= "'" . $_POST['nowa_specjalizacja'] . "' ";
+                    $editQuery .= "'" . $_POST['nowa_specjalizacja'] . "' ";
                 }
-                $kwerenda_edycji .= "WHERE id_nazwiska='" . $_POST['id_nazwiska'] . "'";
-                mysql_query($kwerenda_edycji) or die('B³±d zapytania edycji');
+                $editQuery .= "WHERE id_nazwiska='" . $_POST['id_nazwiska'] . "'";
+                mysql_query($editQuery) or die('B³±d zapytania edycji');
                 echo "<i>Edytowano rekord u¿ytkownika: " . $_POST['nowe_nazwisko'] . " " . $_POST['nowe_imie'] . "</i>";
             }
             elseif(isset($_POST['dodane_nazwisko']) && isset($_POST['dodane_imie']) && isset($_POST['dodane_haslo']) && isset($_POST['dodany_email']) && isset($_POST['dodane_uprawnienia'])){
-                $kwerenda_dodania = "INSERT INTO nazwiska (email,haslo,nazwisko,imie,uprawnienia,specjalizacja) VALUES ";
-                $kwerenda_dodania .= "(";
-                $kwerenda_dodania .= "'" . $_POST['dodany_email'] . "'" . ",";
-                $kwerenda_dodania .= "'" . md5($_POST['dodane_haslo']) . "'" . ",";
-                $kwerenda_dodania .= "'" . $_POST['dodane_nazwisko'] . "'" . ",";
-                $kwerenda_dodania .= "'" . $_POST['dodane_imie'] . "'" . ",";
-                $kwerenda_dodania .= "'" . $_POST['dodane_uprawnienia'] . "'"  . ",";
+                $addQuery = "INSERT INTO nazwiska (email,haslo,nazwisko,imie,uprawnienia,specjalizacja) VALUES ";
+                $addQuery .= "(";
+                $addQuery .= "'" . $_POST['dodany_email'] . "'" . ",";
+                $addQuery .= "'" . md5($_POST['dodane_haslo']) . "'" . ",";
+                $addQuery .= "'" . $_POST['dodane_nazwisko'] . "'" . ",";
+                $addQuery .= "'" . $_POST['dodane_imie'] . "'" . ",";
+                $addQuery .= "'" . $_POST['dodane_uprawnienia'] . "'"  . ",";
                 if($_POST['dodana_specjalizacja'] == ""){
-                    $kwerenda_dodania .= "NULL";
+                    $addQuery .= "NULL";
                 }
                 elseif(($_POST['dodane_uprawnienia'] == "pacjent") || ($_POST['dodane_uprawnienia'] == "admin")){
-                    $kwerenda_dodania .= "NULL";
+                    $addQuery .= "NULL";
                 }
                 else {
-                    $kwerenda_dodania .= "'" . $_POST['dodana_specjalizacja'] . "'";
+                    $addQuery .= "'" . $_POST['dodana_specjalizacja'] . "'";
                 }
-                $kwerenda_dodania .= ")";
-                mysql_query($kwerenda_dodania) or die('B³±d zapytania dodania');
+                $addQuery .= ")";
+                mysql_query($addQuery) or die('B³±d zapytania dodania');
                 echo "<i>Dodano u¿ytkownika: " . $_POST['dodane_nazwisko'] . " " . $_POST['dodane_imie'] . "</i>";
             }
             elseif(isset($_POST['usun']) && ($_POST['usun'] != 1) && ($_POST['usun'] != 17)){
-                $kwerenda_usuniecia = "DELETE FROM nazwiska WHERE id_nazwiska=" . $_POST['usun'] . ";";
-                mysql_query($kwerenda_usuniecia) or die('B³±d zapytania usuniêcia');
+                $removeQuery = "DELETE FROM nazwiska WHERE id_nazwiska=" . $_POST['usun'] . ";";
+                mysql_query($removeQuery) or die('B³±d zapytania usuniêcia');
                 echo "<i>Usuniêto u¿ytkownika o ID_Nazwiska: " . $_POST['usun'] . "</i>";
             }
 
             echo "<br><br>";
-            $forma_dodania = "<br><fieldset><legend>Dodaj u¿ytkownika o podanych parametrach - pamiêtaj ¿e pacjent/admin nie posiadaj± specjalizacji!</legend><form action = \"edit-user.php\" method=\"POST\"> ";
-            $forma_dodania .= "<input type=\"text\" name=\"dodane_nazwisko\" placeholder=\"Nazwisko\"><br>";
-            $forma_dodania .= "<input type=\"text\" name=\"dodane_imie\" placeholder=\"Imiê\"><br>";
-            $forma_dodania .= "<input type=\"password\" name=\"dodane_haslo\" placeholder=\"Has³o\"><br>";
-            $forma_dodania .= "<input type=\"email\" name=\"dodany_email\" placeholder=\"Email\"><br>";
-            $forma_dodania .= "<fieldset><legend>Specjalizacja:</legend><input type=\"radio\" name=\"dodana_specjalizacja\" id=\"dodana_specjalizacja\" value=\"Interna\"/><label for=\"Internista\">Internista</label><br>";
-            $forma_dodania .= "<input type=\"radio\" name=\"dodana_specjalizacja\" id=\"dodana_specjalizacja\" value=\"Ginekolog\"/><label for=\"Ginekolog\">Ginekologia</label><br>";
-            $forma_dodania .= "<input type=\"radio\" name=\"dodana_specjalizacja\" id=\"dodana_specjalizacja\" value=\"USG\"/><label for=\"USG\">USG</label></fieldset><br>";
-            $forma_dodania .= "<fieldset><legend>Status:</legend><input type=\"radio\" name=\"dodane_uprawnienia\" id=\"dodane_uprawnienia\" value=\"admin\"/><label for=\"admin\">Administrator</label><br>";
-            $forma_dodania .= "<input type=\"radio\" name=\"dodane_uprawnienia\" id=\"dodane_uprawnienia\" value=\"lekarz\"/><label for=\"lekarz\">Lekarz</label><br>";
-            $forma_dodania .= "<input type=\"radio\" name=\"dodane_uprawnienia\" id=\"dodane_uprawnienia\" value=\"pacjent\"/><label for=\"pacjent\">Pacjent</label></fieldset><br>";
-            $forma_dodania .= "<input type=\"submit\" value=\"Dodaj rekord\" >";
-            $forma_dodania .= "<input type=\"reset\" value=\"Resetuj dane\" /></fieldset>";
-            $forma_dodania .= "</form><br>";
-            echo $forma_dodania;
+            $addForm = "<br><fieldset><legend>Dodaj u¿ytkownika o podanych parametrach:</legend><form action = \"edit-user.php\" method=\"POST\"> ";
+            $addForm .= "<input type=\"text\" name=\"dodane_nazwisko\" placeholder=\"Nazwisko\"><br>";
+            $addForm .= "<input type=\"text\" name=\"dodane_imie\" placeholder=\"Imiê\"><br>";
+            $addForm .= "<input type=\"password\" name=\"dodane_haslo\" placeholder=\"Has³o\"><br>";
+            $addForm .= "<input type=\"email\" name=\"dodany_email\" placeholder=\"Email\"><br>";
+            $addForm .= "<fieldset><legend>Specjalizacja (pamiêtaj ¿e pacjent/admin nie posiadaj± specjalizacji!):</legend><input type=\"radio\" name=\"dodana_specjalizacja\" id=\"dodana_specjalizacja\" value=\"Interna\"/><label for=\"Internista\">Internista</label><br>";
+            $addForm .= "<input type=\"radio\" name=\"dodana_specjalizacja\" id=\"dodana_specjalizacja\" value=\"Ginekolog\"/><label for=\"Ginekolog\">Ginekologia</label><br>";
+            $addForm .= "<input type=\"radio\" name=\"dodana_specjalizacja\" id=\"dodana_specjalizacja\" value=\"USG\"/><label for=\"USG\">USG</label></fieldset><br>";
+            $addForm .= "<fieldset><legend>Status:</legend><input type=\"radio\" name=\"dodane_uprawnienia\" id=\"dodane_uprawnienia\" value=\"admin\"/><label for=\"admin\">Administrator</label><br>";
+            $addForm .= "<input type=\"radio\" name=\"dodane_uprawnienia\" id=\"dodane_uprawnienia\" value=\"lekarz\"/><label for=\"lekarz\">Lekarz</label><br>";
+            $addForm .= "<input type=\"radio\" name=\"dodane_uprawnienia\" id=\"dodane_uprawnienia\" value=\"pacjent\"/><label for=\"pacjent\">Pacjent</label></fieldset><br>";
+            $addForm .= "<input type=\"submit\" value=\"Dodaj rekord\" >";
+            $addForm .= "<input type=\"reset\" value=\"Resetuj dane\" /></fieldset>";
+            $addForm .= "</form><br>";
+            echo $addForm;
 
-            $kwerenda = "SELECT id_nazwiska, email, haslo, imie, nazwisko, specjalizacja, uprawnienia FROM nazwiska WHERE 1";
-            $wynik = mysql_query($kwerenda) or die('B³±d zapytania');
+            $query = "SELECT id_nazwiska, email, haslo, imie, nazwisko, specjalizacja, uprawnienia FROM nazwiska WHERE 1";
+            $result = mysql_query($query) or die('B³±d zapytania');
 
-            if($wynik){
+            if($result){
                 ?>
                 <table align="center" cellpadding="5" border="1">
                     <tr>
@@ -82,53 +82,53 @@
                         <td style="text-align: center;">Dane u¿ytkowników znajduj±cych siê w bazie danych:</td>
                     </tr>
                 <?
-                while($wiersz = mysql_fetch_assoc($wynik)){
+                while($line = mysql_fetch_assoc($result)){
                     ?>
                     <tr>
                     <?
-                    echo "<td>" . $wiersz['id_nazwiska'] . "</td>";
-                    $forma_edycji = "<td><form action = \"edit-user.php\" method=\"POST\"> ";
-                    $forma_edycji .= "<input type=\"hidden\" name=\"id_nazwiska\" value=\"" . $wiersz['id_nazwiska'] . "\">";
-                    $forma_edycji .= "<input type=\"text\" name=\"nowe_nazwisko\" value=\"" . $wiersz['nazwisko'] . "\">";
-                    $forma_edycji .= "<input type=\"text\" name=\"nowe_imie\" value=\"" . $wiersz['imie'] . "\">";
-                    $forma_edycji .= "<input type=\"text\" name=\"nowe_haslo\" value=\"" . $wiersz['haslo'] . "\" disabled>";
-                    $forma_edycji .= "<input type=\"email\" name=\"nowy_email\" value=\"" . $wiersz['email'] . "\">";
-                    $forma_edycji .= "<select name=\"nowa_specjalizacja\"";
-                    if(($wiersz['uprawnienia'] == "pacjent") || ($wiersz['uprawnienia'] == "admin")){
-                        $forma_edycji .= " disabled";
+                    echo "<td>" . $line['id_nazwiska'] . "</td>";
+                    $editForm = "<td><form action = \"edit-user.php\" method=\"POST\"> ";
+                    $editForm .= "<input type=\"hidden\" name=\"id_nazwiska\" value=\"" . $line['id_nazwiska'] . "\">";
+                    $editForm .= "<input type=\"text\" name=\"nowe_nazwisko\" value=\"" . $line['nazwisko'] . "\">";
+                    $editForm .= "<input type=\"text\" name=\"nowe_imie\" value=\"" . $line['imie'] . "\">";
+                    $editForm .= "<input type=\"text\" name=\"nowe_haslo\" value=\"" . $line['haslo'] . "\" disabled>";
+                    $editForm .= "<input type=\"email\" name=\"nowy_email\" value=\"" . $line['email'] . "\">";
+                    $editForm .= "<select name=\"nowa_specjalizacja\"";
+                    if(($line['uprawnienia'] == "pacjent") || ($line['uprawnienia'] == "admin")){
+                        $editForm .= " disabled";
                     }
-                    $forma_edycji .= ">";
-                    if($wiersz['specjalizacja'] == "USG"){
-                        $forma_edycji .= "<option value=\"USG\" selected=\"selected\">USG</option>";
-                        $forma_edycji .= "<option value=\"Interna\" >Interna</option>";
-                        $forma_edycji .= "<option value=\"Ginekolog\" >Ginekologia</option>";
+                    $editForm .= ">";
+                    if($line['specjalizacja'] == "USG"){
+                        $editForm .= "<option value=\"USG\" selected=\"selected\">USG</option>";
+                        $editForm .= "<option value=\"Interna\" >Interna</option>";
+                        $editForm .= "<option value=\"Ginekolog\" >Ginekologia</option>";
                     }
-                    elseif($wiersz['specjalizacja'] == "Interna"){
-                        $forma_edycji .= "<option value=\"USG\" >USG</option>";
-                        $forma_edycji .= "<option value=\"Interna\" selected=\"selected\">Interna</option>";
-                        $forma_edycji .= "<option value=\"Ginekolog\" >Ginekologia</option>";
+                    elseif($line['specjalizacja'] == "Interna"){
+                        $editForm .= "<option value=\"USG\" >USG</option>";
+                        $editForm .= "<option value=\"Interna\" selected=\"selected\">Interna</option>";
+                        $editForm .= "<option value=\"Ginekolog\" >Ginekologia</option>";
                     }
-                    elseif($wiersz['specjalizacja'] == "Ginekolog"){
-                        $forma_edycji .= "<option value=\"USG\" >USG</option>";
-                        $forma_edycji .= "<option value=\"Interna\" >Interna</option>";
-                        $forma_edycji .= "<option value=\"Ginekolog\" selected=\"selected\">Ginekologia</option>";
+                    elseif($line['specjalizacja'] == "Ginekolog"){
+                        $editForm .= "<option value=\"USG\" >USG</option>";
+                        $editForm .= "<option value=\"Interna\" >Interna</option>";
+                        $editForm .= "<option value=\"Ginekolog\" selected=\"selected\">Ginekologia</option>";
                     }
                     else{
-                        $forma_edycji .= "<option value=\"NULL\" ></option>";
-                        $forma_edycji .= "<option value=\"USG\" >USG</option>";
-                        $forma_edycji .= "<option value=\"Interna\" >Interna</option>";
-                        $forma_edycji .= "<option value=\"Ginekolog\" >Ginekologia</option>";
+                        $editForm .= "<option value=\"NULL\" ></option>";
+                        $editForm .= "<option value=\"USG\" >USG</option>";
+                        $editForm .= "<option value=\"Interna\" >Interna</option>";
+                        $editForm .= "<option value=\"Ginekolog\" >Ginekologia</option>";
                     }
-                    $forma_edycji .= "</select>";
-                    $forma_edycji .= "<input type=\"text\" name=\"uprawnienia\" value=\"" . $wiersz['uprawnienia'] . " \"disabled>";
-                    $forma_edycji .= "<input type=\"submit\" value=\"Edytuj rekord\" >";
-                    $forma_edycji .= "</form></td>";
-                    echo $forma_edycji;
+                    $editForm .= "</select>";
+                    $editForm .= "<input type=\"text\" name=\"uprawnienia\" value=\"" . $line['uprawnienia'] . " \"disabled>";
+                    $editForm .= "<input type=\"submit\" value=\"Edytuj rekord\" >";
+                    $editForm .= "</form></td>";
+                    echo $editForm;
 
-                    $form_usun = "<td><form action=\"edit-user.php\" method=\"POST\">";
-                    $form_usun .= "<button name=\"usun\" type=\"submit\" value=\"" . $wiersz['id_nazwiska'] . "\">Usuñ rekord</button>";
-                    $form_usun .= "</form></td>";
-                    echo $form_usun;
+                    $removeForm = "<td><form action=\"edit-user.php\" method=\"POST\">";
+                    $removeForm .= "<button name=\"usun\" type=\"submit\" value=\"" . $line['id_nazwiska'] . "\">Usuñ rekord</button>";
+                    $removeForm .= "</form></td>";
+                    echo $removeForm;
                     ?>
                     </tr>
                     <?
--- src/gabinet.php
+++ src/gabinet.php
@@ -25,29 +25,30 @@
             while((date_format($_SESSION['data'],'l'))!="Monday"){
                 date_modify($_SESSION['data'], '-1 day');
             }
-            $dataKoniec = clone $_SESSION['data'];
-            date_modify($dataKoniec, '+4 day');
-            echo "Pocz±tek tygodnia:" . date_format($_SESSION['data'], 'Y-m-d') . "<br>Koniec tygodnia:" . date_format($dataKoniec, 'Y-m-d') . "<br>";
+            $endDate = clone $_SESSION['data'];
+            date_modify($endDate, '+4 day');
+            echo "Pocz±tek tygodnia:" . date_format($_SESSION['data'], 'Y-m-d') . "<br>Koniec tygodnia:" . date_format($endDate, 'Y-m-d') . "<br>";
             ?>
             <form action="gabinet.php" method="POST">
 			    <input type="submit" value="Tydzieñ wstecz" name="Wstecz" />
                 <input type="submit" value="Tydzieñ w przód" name="Dalej" />
             </form>
-            <br> Zajmij gabinet <br>
             <?
-            echo "<form action=\"gabinet.php\" method=\"POST\">";
+            echo "<br><fieldset><legend>Zajmij gabinet:</legend><form action=\"gabinet.php\" method=\"POST\">";
             if(isset($_POST['Dzien'])){
                 $_SESSION['Dzien'] = $_POST['Dzien'];
-                echo "Godzina rozpoczêcia "."<select name=\"GodzinaRozpoczecia\">";
+                echo "Godzina rozpoczêcia " . "<select name=\"GodzinaRozpoczecia\">";
                 generateDate(date_create('7:00'), date_create('19:00'));
-                echo "<br><input type=\"submit\" value=\"Dalej\" /></form>";
-            }elseif(isset($_POST['GodzinaRozpoczecia'])){
+                echo "<br><input type=\"submit\" value=\"Dalej\" /></form></fieldset>";
+            }
+            elseif(isset($_POST['GodzinaRozpoczecia'])){
                 $_SESSION['GodzinaRozpoczecia'] = $_POST['GodzinaRozpoczecia'];
-                $poczatek = date_create($_SESSION['GodzinaRozpoczecia']);
-                echo "</select>"."Godzina zakoñczenia "."<select name=\"GodzinaZakonczenia\">";
-                generateDate(date_modify($poczatek,'+2 hours'), date_create('21:00'));
-                echo "<br><input type=\"submit\" value=\"Zajmij\" /></form>";
-            }else{
+                $timeBegin = date_create($_SESSION['GodzinaRozpoczecia']);
+                echo "</select>" . "Godzina zakoñczenia " . "<select name=\"GodzinaZakonczenia\">";
+                generateDate(date_modify($timeBegin,'+2 hours'), date_create('21:00'));
+                echo "<br><input type=\"submit\" value=\"Zajmij\" /></form></fieldset>";
+            }
+            else{
                 if(isset($_POST['GodzinaZakonczenia'])){
                     echo $_SESSION['Dzien']."<br>";
                     echo $_SESSION['GodzinaRozpoczecia']."<br>";
@@ -67,7 +68,7 @@
                 echo "<td><input type=\"radio\" name=\"Dzien\" value=\"Sro\">¦roda</td>";
                 echo "<td><input type=\"radio\" name=\"Dzien\" value=\"Czw\">Czwartek</td>";
                 echo "<td><input type=\"radio\" name=\"Dzien\" value=\"Pia\">Pi±tek<br></td>";
-                echo "</tr></table><input type=\"submit\" value=\"Dalej\" /><br><br></form>";
+                echo "</tr></table><input type=\"submit\" value=\"Dalej\" /><br><br></form></fieldset>";
             }
 
             // TODO Dorobiæ zapisy/modyfikacjê zajmowania gabinetów
@@ -77,26 +78,26 @@
             // TODO jak po tym wszystkim nie jest zajêta to mo¿na dodaæ nowy rekord rezerwacji gabinetu
             // TODO zapytanie o wszystkie gabinety od dni do dnia z wy¶wietleniem jego specjalizacji - je¿eli jest wolny siê wy¶wietli, w przeciwnym wypadku ignoruj
             // TODO zajêcie gabinetu w danym dniu nie krócej ni¿ 2h nie d³u¿ej ni¿ 8h
-            $kwerenda_przegladania_gab = "SELECT ID_gabinetu FROM zajetosc";
-            $wynik_gab = mysql_query($kwerenda_przegladania_gab) or die('B³±d zapytania');
-            $forma_przegladania_gab = "<fieldset><legend>Przejrzyj zajêto¶æ gabinet:</legend><form action = \"gabinet.php\" method=\"POST\">";
-            $forma_przegladania_gab .= "Gabinet: <select name=\"ID_przegladany_gabinet\">";
-            if($wynik_gab) {
+            $docOfficeViewQuery = "SELECT ID_gabinetu FROM zajetosc";
+            $viewOfficeResult = mysql_query($docOfficeViewQuery) or die('B³±d zapytania');
+            $docOfficeViewFrom = "<br><fieldset><legend>Przejrzyj zajêto¶æ gabinet:</legend><form action = \"gabinet.php\" method=\"POST\">";
+            $docOfficeViewFrom .= "Gabinet: <select name=\"ID_przegladany_gabinet\">";
+            if($viewOfficeResult) {
                 $iterator = 0;
-                while($wiersz_gab = mysql_fetch_assoc($wynik_gab)) {
-                    $tablica_gabinetow[$iterator] = $wiersz_gab['ID_gabinetu'];
+                while($docOfficeViewLine = mysql_fetch_assoc($viewOfficeResult)) {
+                    $docOfficeTable[$iterator] = $docOfficeViewLine['ID_gabinetu'];
                     $iterator = $iterator + 1;
                 }
-                sort($tablica_gabinetow);
-                $tablica_gabinetow = array_unique($tablica_gabinetow);
-                foreach($tablica_gabinetow as $key => $val){
-                    $forma_przegladania_gab .= "<option value=\"" . $val . "\">" . $val . "</option>";
+                sort($docOfficeTable);
+                $docOfficeTable = array_unique($docOfficeTable);
+                foreach($docOfficeTable as $key => $val){
+                    $docOfficeViewFrom .= "<option value=\"" . $val . "\">" . $val . "</option>";
                 }
             }
-            $forma_przegladania_gab .= "</select>";
-            $forma_przegladania_gab .= "<input type=\"submit\" value=\"Przegl±daj gabinet\" >";
-            $forma_przegladania_gab .= "</form>";
-            echo $forma_przegladania_gab;
+            $docOfficeViewFrom .= "</select>";
+            $docOfficeViewFrom .= "<input type=\"submit\" value=\"Przegl±daj gabinet\" >";
+            $docOfficeViewFrom .= "</form>";
+            echo $docOfficeViewFrom;
 
             if(isset($_POST['ID_przegladany_gabinet'])){
                 $_SESSION['ID_przegladany_gabinet'] = $_POST['ID_przegladany_gabinet'];
@@ -104,7 +105,7 @@
 
             echo "Przegl±dany gabinet: " . $_SESSION['ID_przegladany_gabinet'];
             drawTable(clone $_SESSION['data'], $_SESSION['ID_przegladany_gabinet']);
-
+            echo "</fieldset>";
 		}
 		else {
 			echo "Nie posiadasz uprawnieñ lekarza";
