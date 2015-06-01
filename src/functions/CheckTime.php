893e089152a4f309f424cd8c7257b69ff661b9ec
Zaeus <wpb1992@gmail.com>
2015-06-01 17:26:01 +0200

    Ogarnianie burdelu v2

--- src/functions/CheckTime.php
+++ src/functions/CheckTime.php
@@ -1,27 +1,28 @@
 <?
 // Funkcja sprawdzająca zajętość dnia oraz wyświetlająca tą wynik w postaci pokolorowanej komórki tabeli
-function checkTime($day, $hour, $date, $idGabinetu) {
+// Komórka jest zielona w przypadku braku znalezienia pasującego do wytycznych rekordu
+// Komórka jest czerwona z wypisanym nazwiskiem lekarza w przypadku znalezienia pasującego do wytycznych rekordu
+function checkTime($day, $time, $date, $idGabinetu) {
 
-    $hour = date_format($hour, 'H:i:s');
+    $time = date_format($time, 'H:i:s');
     $date = date_format($date, 'Y-m-d');
 
-    $kwerenda_checkTime = "SELECT ID_nazwiska_Lek, od_dnia, do_dnia, od_godziny, do_godziny FROM zajetosc WHERE dzien_tyg='" . $day . "' AND ID_gabinetu='" . $idGabinetu . "'";
-    $wynik_checkTime = mysql_query($kwerenda_checkTime) or die('Błąd zapytania');
-    if ($wynik_checkTime) {
-        // TODO kwerenda pobierająca nazwisko lekarza z tablicy nazwisk
-        unset($tab_wiersz);
-        while ($wiersz = mysql_fetch_assoc($wynik_checkTime)) {
-            if (($wiersz['od_godziny'] <= $hour) && ($wiersz['do_godziny'] >= $hour) && ($wiersz['od_dnia'] <= $date) && ($wiersz['do_dnia'] >= $date)) {
-                $kwerenda_Lekarz = "SELECT nazwisko FROM nazwiska WHERE id_nazwiska='" . $wiersz['ID_nazwiska_Lek'] . "'";
-                $wynik_Lekarz = mysql_query($kwerenda_Lekarz) or die('Błąd zapytania');
-                $dane_Lekarz = mysql_fetch_assoc($wynik_Lekarz);
-                $tab_wiersz = "<td width=\"100\" bgcolor=\"red\" style=\"text-align: center\">" . $dane_Lekarz['nazwisko'] . "</td>";
+    $queryCheckTime = "SELECT ID_nazwiska_Lek, od_dnia, do_dnia, od_godziny, do_godziny FROM zajetosc WHERE dzien_tyg='" . $day . "' AND ID_gabinetu='" . $idGabinetu . "'";
+    $resultCheckTime = mysql_query($queryCheckTime) or die('Błąd zapytania');
+    if ($resultCheckTime) {
+        unset($tabRecord);
+        while ($line = mysql_fetch_assoc($resultCheckTime)) {
+            if (($line['od_godziny'] <= $time) && ($line['do_godziny'] >= $time) && ($line['od_dnia'] <= $date) && ($line['do_dnia'] >= $date)) {
+                $queryDoc = "SELECT nazwisko FROM nazwiska WHERE id_nazwiska='" . $line['ID_nazwiska_Lek'] . "'";
+                $docResult = mysql_query($queryDoc) or die('Błąd zapytania');
+                $docData = mysql_fetch_assoc($docResult);
+                $tabRecord = "<td width=\"100\" bgcolor=\"red\" style=\"text-align: center\">" . $docData['nazwisko'] . "</td>";
             }
         }
     }
-    if (!isset($tab_wiersz)) {
-        $tab_wiersz = "<td width=\"100\" bgcolor=\"green\" style=\"text-align: center\"></td>";
+    if (!isset($tabRecord)) {
+        $tabRecord = "<td width=\"100\" bgcolor=\"green\" style=\"text-align: center\"></td>";
     }
-    echo $tab_wiersz;
+    echo $tabRecord;
 }
 ?>
--- src/functions/drawTable.php
+++ src/functions/drawTable.php
@@ -1,72 +1,71 @@
 <?
-function drawTable($data, $idGabinetu){
-    $dataWyswietlanieDni = clone $data;
+function drawTable($date, $idGabinetu){
+    $dateShowDay = clone $date;
     if(isset($idGabinetu)){
 ?>
-<br>
-<table align="center" cellpadding="5" border="1">
-    <tr bgcolor>
-        <td style="text-align: center;">Godzina</td>
-        <td style="text-align: center;">Poniedzia³ek<br>
-            <?
-            echo date_format($dataWyswietlanieDni, 'Y-m-d');
-            ?>
-        </td>
-        <td style="text-align: center;">Wtorek<br>
-            <?
-            echo date_format(date_modify($dataWyswietlanieDni, '+1 day'), 'Y-m-d');
-            ?>
-        </td>
-        <td style="text-align: center;">¦roda<br>
-            <?
-            echo date_format(date_modify($dataWyswietlanieDni, '+1 day'), 'Y-m-d');
-            ?>
-        </td>
-        <td style="text-align: center;">Czwartek<br>
-            <?
-            echo date_format(date_modify($dataWyswietlanieDni, '+1 day'), 'Y-m-d');
-            ?>
-        </td>
-        <td style="text-align: center;">Pi±tek<br>
-            <?
-            echo date_format(date_modify($dataWyswietlanieDni, '+1 day'), 'Y-m-d');
-            ?>
-        </td>
-    </tr>
-    <?
-    $godzina = date_create('7:00');
-    while($godzina!=date_create('21:30')) {
-        // TODO Kwerendy z zapytaniami dla danej godziny dla danego dnia - jak jest to kolorowanie czerwony dla zajêtego
-        // TODO plus pobranie nazwiska lekarza i jego wy¶wietlenie na polu
-        // TODO w przeciwnym wypadku kolor zielony bez niczego
-        ?>
-        <tr bgcolor=white>
-            <td width="100" style="text-align: center;">
+    <br>
+    <table align="center" cellpadding="5" border="1">
+        <tr bgcolor>
+            <td style="text-align: center;">Godzina</td>
+            <td style="text-align: center;">Poniedzia³ek<br>
                 <?
-                echo date_format($godzina,'H:i');
+                echo date_format($dateShowDay, 'Y-m-d');
                 ?>
             </td>
-            <?
-            // Sprawdzenie poniedzia³ku
-            checkTime("Pon",$godzina,$data,$idGabinetu);
-            date_modify($data, '+1 day');
-            // Sprawdzenie wtorku
-            checkTime("Wto",$godzina,$data,$idGabinetu);
-            date_modify($data, '+1 day');
-            // Sprawdzenie ¶rody
-            checkTime("Sro",$godzina,$data,$idGabinetu);
-            date_modify($data, '+1 day');
-            // Sprawdzenie czwartku
-            checkTime("Czw",$godzina,$data,$idGabinetu);
-            date_modify($data, '+1 day');
-            // Sprawdzenie pi±tku
-            checkTime("Pia",$godzina,$data,$idGabinetu);
-            date_modify($data, '-4 days')
-            ?>
+            <td style="text-align: center;">Wtorek<br>
+                <?
+                echo date_format(date_modify($dateShowDay, '+1 day'), 'Y-m-d');
+                ?>
+            </td>
+            <td style="text-align: center;">¦roda<br>
+                <?
+                echo date_format(date_modify($dateShowDay, '+1 day'), 'Y-m-d');
+                ?>
+            </td>
+            <td style="text-align: center;">Czwartek<br>
+                <?
+                echo date_format(date_modify($dateShowDay, '+1 day'), 'Y-m-d');
+                ?>
+            </td>
+            <td style="text-align: center;">Pi±tek<br>
+                <?
+                echo date_format(date_modify($dateShowDay, '+1 day'), 'Y-m-d');
+                ?>
+            </td>
         </tr>
         <?
-        $godzina = date_modify($godzina, '+30 minutes');
+        $time = date_create('7:00');
+        while($time != date_create('21:30')) {
+            ?>
+            <tr bgcolor=white>
+                <td width="100" style="text-align: center;">
+                    <?
+                    echo date_format($time,'H:i');
+                    ?>
+                </td>
+                <?
+                // Sprawdzenie poniedzia³ku
+                checkTime("Pon", $time, $date, $idGabinetu);
+                date_modify($date, '+1 day');
+                // Sprawdzenie wtorku
+                checkTime("Wto", $time, $date, $idGabinetu);
+                date_modify($date, '+1 day');
+                // Sprawdzenie ¶rody
+                checkTime("Sro", $time, $date, $idGabinetu);
+                date_modify($date, '+1 day');
+                // Sprawdzenie czwartku
+                checkTime("Czw", $time, $date, $idGabinetu);
+                date_modify($date, '+1 day');
+                // Sprawdzenie pi±tku
+                checkTime("Pia", $time, $date, $idGabinetu);
+                date_modify($date, '-4 days')
+                ?>
+            </tr>
+            <?
+            $time = date_modify($time, '+30 minutes');
+        }
+        ?>
+    </table>
+    <?
     }
-    ?>
-</table>
-<?}}
+}
