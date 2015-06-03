502f8c786a024050ff24e944415bb633e0e27264
Zaeus <wpb1992@gmail.com>
2015-06-03 20:21:55 +0200

    Zapisy v0.2

--- src/functions/signUpForDoc.php
+++ src/functions/signUpForDoc.php
@@ -1,8 +1,10 @@
 <?php
+include("GenerateDate.php");
+
 function SignUpForDoc($officeSpecialization)
 {
     // TODO tabele z wygenerowanymi parametrami gabinetów po pobraniu info o szukanej spracjalno¶ci gabinetu (ew. mie¶cie)
-
+    echo "<br><fieldset><legend>Dostêpne gabinety:</legend>";
     $officeSpecQuery = "SELECT zajetosc.ID_nazwiska_Lek, zajetosc.dzien_tyg, zajetosc.od_dnia, zajetosc.do_dnia, zajetosc.od_godziny, zajetosc.do_godziny, budynki.miasto FROM gabinety ";
     $officeSpecQuery .= "INNER JOIN zajetosc ON gabinety.ID_gabinetu = zajetosc.ID_gabinetu ";
     $officeSpecQuery .= "INNER JOIN budynki ON gabinety.ID_budynku = budynki.ID_budynku ";
@@ -10,7 +12,7 @@ function SignUpForDoc($officeSpecialization)
     $officeSpecResult = mysql_query($officeSpecQuery) or die('B³±d zapytania o gabinety o podanej specjalizacji');
     $howMuchLines = mysql_num_rows($officeSpecResult);
     if($howMuchLines > 0) {
-        echo "<br><fieldset><legend>Dostêpne gabinety:</legend><table align=\"center\" cellpadding=\"5\" border=\"1\">";
+        echo "<table align=\"center\" cellpadding=\"5\" border=\"1\">";
         echo "<td>Dane lekarza</td>";
         echo "<td>Miasto</td>";
         echo "<td>Dzieñ tygodnia</td>";
@@ -36,14 +38,18 @@ function SignUpForDoc($officeSpecialization)
             // TODO selektor ma usuniête godziny z zaklepanych godzin
             // TODO Poza selektoram powinien byæ wybór daty z zakresu najmu gabinetu
             // TODO przycisk zarezerwowania wizyty
-            /* echo "<input type=\"select\" name=\"GodzinaRezerwacji\" value=\"" .  . "\">"; */
+            echo date_format($officeSpecLine['do_godziny'],'H:i');
+            echo "<select name=\"godzinaRezerwacji\">";
+            generateDate(date_create($officeSpecLine['od_godziny']), date_modify(date_create($officeSpecLine['do_godziny']), '-30 minutes'));
+            echo "</select>";
             echo "<input type=\"submit\" value=\"Rezerwuj\" ></form></td>";
             echo "</tr>";
             echo "</tr>";
         }
-        echo "</table><br></fieldset>";
+        echo "</table>";
     } else {
         echo "Brak gabinetów o podanej specjalizacji";
     }
+    echo "</fieldset><br>";
 }
 ?>
--- src/zapis.php
+++ src/zapis.php
@@ -7,13 +7,25 @@
 	include("includes/kwerenda_log.php");
     include("functions/signUpForDoc.php");
 ?>
-
-//Zapis do lekarza
-
 <?
 	if(isset($_SESSION['login']) && ($_SESSION['haslo'] == $hasloSql)){
-		// Edycja istniej±cych zapisów z mo¿liwo¶ci± ich usuwania
-        SignUpForDoc("USG");
+        echo "<fieldset><legend>Specjalizacja gabinetu:</legend>";
+        echo "<form action = \"zapis.php\" method=\"POST\"> ";
+        echo "<select name=\"specjalizacjaGabinetu\">";
+        echo "<option value=\"USG\" >USG</option>";
+        echo "<option value=\"Interna\" >Interna</option>";
+        echo "<option value=\"Ginekolog\" >Ginekologia</option>";
+        echo "</select>";
+        echo "<input type=\"submit\" value=\"Wybierz\" >";
+        echo "</form></td>";
+        echo "</fieldset>";
+        if(isset($_POST['specjalizacjaGabinetu'])) {
+            SignUpForDoc($_POST['specjalizacjaGabinetu']);
+        }
+        if(isset($_POST['godzinaRezerwacji'])){
+            // TODO obs³uga POST z funkcji SignUpForDoc
+        }
+        // TODO Edycja istniej±cych zapisów z mo¿liwo¶ci± ich usuwania
 	}
 	else{
 		echo "Brak uprawnieñ do tre¶ci.<br>";
